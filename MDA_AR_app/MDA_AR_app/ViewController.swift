80b4a4c0962128a6e579ec552e55df81c118626c
Yura <kibernetic@protonmail.com>
2020-09-19 19:40:45 +0300

    Добавил новую сцену

---
+++ MDA_AR_app/MDA_AR_app/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MDA_AR_app
+//
+//  Created by Master on 19.09.2020.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
